09db0192b7648fe58e972cf07076cb369f9c0e74
justinYoo <ugh1118@gmail.com>
2024-02-28 17:06:45 +0900

    [test] write basic test codes
    [test] write basic test codes

---
+++ src/test/java/com/jiyoo/springwebatoz/web/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package com.jiyoo.springwebatoz.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+
+    @Test
+    void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 10000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)));
+        //TODO: 추후 JUnit5 이용한 코드로 테스트 필요
+    }
+}
