f556634f57ff5a631e64605a8de2e4a48a5fb3b5
justinYoo <ugh1118@gmail.com>
2024-02-28 12:59:00 +0900

    [Init] project init

---
+++ src/main/java/com/jiyoo/springwebatoz/SpringWebAtozApplication.java
@@ -0,0 +1,13 @@
+package com.jiyoo.springwebatoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAtozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAtozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiyoo/springwebatoz/SpringWebAtozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiyoo.springwebatoz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebAtozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
