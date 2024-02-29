df70e52b1670041ab497b44d4cb1a6567220eb08
justinYoo <ugh1118@gmail.com>
2024-02-29 17:38:24 +0900

    [feat] add main page

---
+++ src/test/java/com/jiyoo/springwebatoz/web/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package com.jiyoo.springwebatoz.web;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void 메인페이지_로딩() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        assertThat(body).contains("Web Service with Spring Boot");
+    }
+
+}
